ac511b01dc22ae287e8ebdfe5339177eddec70c2
Luyoung <luyangkht@163.com>
2014-06-08 20:43:51 +0800

    样式

--- web/config/config.php
+++ web/config/config.php
@@ -3,7 +3,7 @@
 	define( "CLIENT_SECRET" , 'D1A3BE41917BD3A4FDD2A78FB3279D' );
 	define( "CALLBACK_URL" , 'http://localhost:8888/callback.php' );
 	
-	define( "WEB_ROOT" , 'D:/Solution/Hackthon/mdconference/web/' );
+	define( "WEB_ROOT" , 'E:/hackthon/mdconference/web/' );
 	$_config = array();
 	$_config['yuntongxun']=array('url'=>'http://mdconf.mingdao.com/','appid'=>'1234567890');
 
