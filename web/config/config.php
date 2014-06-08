37d9c0a35305db16c2d1b7cd3bc079469b2d7ac0
Luyoung <luyangkht@163.com>
2014-06-09 00:28:15 +0800

    config

--- web/config/config.php
+++ web/config/config.php
@@ -1,6 +1,6 @@
 <?php
-	define( "CLIENT_ID" , 'ED5D26DF5DDA' );
-	define( "CLIENT_SECRET" , 'D1A3BE41917BD3A4FDD2A78FB3279D' );
+	define( "CLIENT_ID" , '852FDC15D0AEE7D7BA18EF752B77' );
+	define( "CLIENT_SECRET" , '4339194819923EAFD8ECF9C3912B2C' );
 	define( "CALLBACK_URL" , 'http://localhost:8888/callback.php' );
 	
 	define( "WEB_ROOT" , 'E:/hackthon/mdconference/web/' );
